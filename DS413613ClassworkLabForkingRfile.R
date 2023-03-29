f6326cc0a68715c6b3884de303c8c1cc83e7fb0a
Sonia Saini (She/her) <97491513+sonias3@users.noreply.github.com>
2023-03-29 19:19:42 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is John Doe
-# My favorite color is blue
+# My name is Sonia Saini
 # This is my coding that generates Boxplots for transmission types
 ggplot(data = mpg) +
-  geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
+  geom_boxplot(mapping = aes(x = displ, y = drv, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
   ylab("displacement") 
-  
+
+#The data source is the mpg data table
 
 
